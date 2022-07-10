42b85c4c5940bb8de71ce2dc9f1ae569907949ae
JashGoyal <jashgoyal4504@gmail.com>
2022-07-10 22:37:29 +0530

    5

---
+++ 05.py
@@ -0,0 +1,18 @@
+a = 3
+b = 4
+print("The value of 3+4 is ", 3+4)
+print("The value of 3-4 is ", 3-4)
+print("The value of 3*4 is ", 3*4)
+print("The value of 3/4 is ", 3/4)
+a = 34
+a -= 12
+a *= 12
+a /= 12
+print(a)
+b = (14!=7)
+print(b)
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is", (bool1 and bool2))
+print("The value of bool1 or bool2 is", (bool1 or bool2))
+print("The value of not bool2 is", (not bool2))
