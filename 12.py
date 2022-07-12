24a9fee93ba6dc5d3af2c53eabcf1351be0d007b
JashGoyal <jashgoyal4504@gmail.com>
2022-07-12 17:52:53 +0530

    12

---
+++ 12.py
@@ -0,0 +1,9 @@
+story = "Hello! my name is Jash and i am working on my python courses"
+
+print(len(story))
+print(story.endswith("my"))
+print(story.count("a"))
+print(story.capitalize())
+print(story.find("working"))
+print(story.replace("Jash", "Jash Goyal"))
+ 
