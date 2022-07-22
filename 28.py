589a855f1162e913d4d3a8f1d3fd5efbfe18dd81
JashGoyal <jashgoyal4504@gmail.com>
2022-07-22 14:02:18 +0530

    28

---
+++ 28.py
@@ -0,0 +1,9 @@
+myDict = {
+    "Fast": "In a Quick Manner",
+    "Jash": "A Coder",
+    "Marks": [1, 2, 5],
+    "anotherdict": {'Jash': 'Player'}
+}
+myDict['Marks'] = [45, 78]
+print(myDict['Marks'])
+print(myDict['anotherdict']['Jash'])
