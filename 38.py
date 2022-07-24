ada60bfe579a409a0de94019d9dcc33f8031868c
JashGoyal <jashgoyal4504@gmail.com>
2022-07-24 22:09:49 +0530

    38

---
+++ 38.py
@@ -0,0 +1,17 @@
+a = 8
+
+if(a<3): 
+    print("The value of a is greater than 3")
+
+if(a>13):
+    print("The value of a is greater than 13")
+    
+if(a>7):
+    print("The value of a is greater than 7")
+
+if(a>17):
+    print("The value of a is greater than 17")
+else:
+    print("The value is not greater than 3 or 7")
+
+print("Done")
