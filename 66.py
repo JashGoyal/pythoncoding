e26789eff8106f36f2925fad46153ede124cc144
JashGoyal <jashgoyal4504@gmail.com>
2022-07-28 23:33:27 +0530

    66

---
+++ 66.py
@@ -0,0 +1,14 @@
+def factorial_iter(n):
+    product = 1
+    for i in range(n):
+        product = product * (i+1)
+    return product
+
+def factorial_recursive(n):
+    if n == 1 or n == 0:
+        return 1
+    return n * factorial_recursive(n-1)
+
+f = factorial_recursive(0)
+print(f)
+
