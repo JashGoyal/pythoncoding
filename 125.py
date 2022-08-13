5364473feefff6172ca52af30c2866facdcfaebf
JashGoyal <jashgoyal4504@gmail.com>
2022-08-13 22:36:15 +0530

    125

--- 125.py
+++ 125.py
@@ -1,11 +1,4 @@
 a = [3, 6, 7, 8, 9, 2, 4, 23, 75, 23, 123, 67]
-# b = []
-# for item in a:
-#     if item%2==0:
-#         b.append(item)
-# print(b)
-
-# Shortcut to write the same:
 b = [i for i in a if i%2==0]
 print(b)
 
